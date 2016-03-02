a28e9a902b9640a50c7c8fddf66f28acbb030b99
Rokic <Rokic.github@gmail.com>
2016-03-02 22:35:15 +0800

    Added files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+SPIDER_ACCOUNT = "spider_at_zhihu@126.com"
+SPIDER_PASSWORD = "spiderwithzhihu"
+
+EMAIL_ACCOUNT = "spider_at_zhihu@126.com"
+EMAIL_PASSWORD = "spider2zhihu"
+
+SUSCRIBER_LIST = ["user@example.com"]
+
+SMTP_SERVER = "smtp.126.com"

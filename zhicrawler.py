9f9855924089689de5deecb40a6e0c824859a817
RainbowWater <tilliforgetall@gmail.com>
2016-03-02 20:30:15 +0800

    Update zhicrawler.py
    Separated configurations to yaml file.
    Trying to support different mail servers (unfortunately not gmail, while the reason is obvious :().

--- zhicrawler.py
+++ zhicrawler.py
@@ -5,33 +5,17 @@ import re
 import smtplib
 from email.mime.text import MIMEText
 import datetime, time
+import yaml
 
-# -- CONFIG -- #
-# A = "Always"
-pattern = re.compile(r'<div id="js-ho.*;}">(.*)<a href=".*>更多<', re.S)
-
-site = "https://www.zhihu.com"
-hdr = {
-    'Connection': 'Keep-Alive',
-    'Accept': 'text/html, application/xhtml+xml, */*',
-    'Accept-Language': 'zh-CN,zh;q=0.8',
-    'User-Agent':
-    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Accept-Encoding': 'gzip,deflate,sdch',
-    'Host': 'www.zhihu.com',
-    'DNT': '1'
-    }
-user = {'account': '',
-        'password': ''}
-mail = {'addr': '',
-        'pwd': ''}
-times = 1
-freq = 3600
-path = r'E:\Downloads\zhic.txt'
+# -- config -- #
+with open(r'./yaml') as f:
+    config = yaml.load(f)
+    
+pattern = re.compile(config['pattern'], re.S)
 
 # -- FUNCTIONALITY -- #
 def login(site, user, hdr):
-    account = user['account']
+    account = str(config['user']['account'])
     login_type = 'email' if '@' in account else 'phone_num'
     session = requests.Session()
 
@@ -77,18 +61,21 @@ def fpl(session, url, path, hdr, times, freq):
 
 def zhiliao(path, mail):
     news = open(path, 'r', encoding='utf-8').read()
-    smtp_server = smtplib.SMTP_SSL("smtp.qq.com", port=465, timeout=30)
-    smtp_server.login(mail['addr'], mail['pwd'])
 
     msg = MIMEText(news)
     msg['Subject'] = "News @zhihu %s" % datetime.datetime.today()
     msg["From"] = msg["To"] = mail['addr']
+
+    target = "smtp.%s" % mail['addr'].split("@")[1]
+    smtp_server = smtplib.SMTP_SSL(target, port=465, timeout=30)
+    smtp_server.login(mail['addr'], mail['pwd'])
+
     
     smtp_server.send_message(msg)
     print("Thy will be done in earth, as it is in heaven.")
 
 # -- MAIN -- #
 if __name__ == "__main__":    
-    session = login(site, user, hdr)
-    fpl(session, site, path, hdr, times, freq)
-    zhiliao(path, mail)
+    session = login(*(config[i] for i in ('site', 'user', 'hdr')))
+    fpl(session, *(config[i] for i in 'site, path, hdr, times, freq'.split(', ')))
+    zhiliao(*(config[i] for i in ('path', 'mail')))
